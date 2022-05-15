0fdca6310d4e95a073907b8baa3a91e5077a5cce
Christopher <christopher2brown@hotmail.co.uk>
2022-05-15 15:30:27 +0100

    finished

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,18 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" for the number 3' do
+    it 'returns "fizzbuzz" for the number 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+
+    it 'returns "fizz" when passed number 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
+   
+
+    it 'return "buzz" when passed number 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+
+   
 end
